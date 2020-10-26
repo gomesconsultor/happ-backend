1f07b4e4ba337f8f1ba1af0dea04879906f3c5c5
Carlos <gomescgconsultoria@gmail.com>
2020-10-26 08:49:13 -0300

    alteração na pasta src

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -2,12 +2,12 @@ import multer from 'multer';
 import path from 'path';
 
 export default {
-    storage: multer.diskStorage({
-        destination: path.join(__dirname, '..', '..', 'uploads'),
-        filename: (request, file, cb) => {
-            const fileName = `${Date.now()}-${file.originalname}`;
-            
-            cb(null, fileName);
-        },
-    })
+  storage: multer.diskStorage({
+    destination: path.resolve(__dirname, '..', '..', 'uploads'),
+    filename: (request, file, callback) => {
+      const fileName = `${Date.now()}-${file.originalname}`;
+
+      callback(null, fileName);
+    },
+  })
 };
--- src/server.ts
+++ src/server.ts
@@ -1,69 +1,20 @@
 import path from 'path';
-import 'dotenv/config';
-import express  from 'express';
 
-import './database/connection';
+import 'dotenv/config';
+import express from 'express';
 import 'express-async-errors';
-import errorHandler from './errors/handler';
-import routes from './routes';
-import multer from 'multer';
 import cors from 'cors';
 
+import './database/connection';
+import routes from './routes';
+import errorHandler from './errors/handler';
 
 const app = express();
+
 app.use(cors());
-app.use(routes);
 app.use(express.json());
-app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(routes);
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use(errorHandler);
 
-app.get('/:id', (request, response) => {
-    console.log(request.query);
-    console.log(request.params);
-    response.json('Seja bem vindo');
-});
-
-//  app.post('/orfanato', async (request, response) => {
-//     const {
-//         name,
-//         latitude,
-//         longitude,
-//         about,
-//         instructions,
-//         opening_hours,
-//         open_on_weekends
-//     } = request.body; 
-
-//     const orphanagesRepository = getRepository(Orphanage);
-
-//     const orphanage = orphanagesRepository.create({
-//         name,
-//         latitude,
-//         longitude,
-//         about,
-//         instructions,
-//         opening_hours,
-//         open_on_weekends
-//     });
-
-//     await orphanagesRepository.save(orphanage);
-
-
-
-
-
-
-
-
-//     console.log(request.body);   
-//     response.status(201).json(request.body);
-// });
-
-
-
-
-//app.use(errorHandler);
-
-app.listen(process.env.PORT || 3333, () => {
-    console.log("executando ");
-})
-
+app.listen(process.env.PORT || 3333);
